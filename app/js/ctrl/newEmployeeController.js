8c7031084797186e2137643302e46db72400afdf
oozcan <onurozcan95@gmail.com>
2022-06-14 11:11:05 -0700

    MAN-7 Yeni Çalışan sayfasına bootbox eklenmesi

--- app/js/ctrl/newEmployeeController.js
+++ app/js/ctrl/newEmployeeController.js
@@ -11,8 +11,25 @@ myApp.controller('newEmployeeController', ['$scope', '$state', '$stateParams','$
         $http.post('/api/employee/save', JSON.stringify(data)).then(function (response) {
         
         });
-        $state.go('home', {})
-    }
+        const bootboxOpts = {};
+		bootboxOpts.title = '';
+        bootboxOpts.message = 'Çalışan Kaydedilmiştir!';
+        bootboxOpts.centerVertical  = true;
+        bootboxOpts.closeButton = false;
+						
+		bootboxOpts.buttons = {
+			cancel: { label: 'Anasayfa', className: 'btn-danger' },
+			confirm: { label: 'Çalışan Detayına Git', className: 'btn-success' }
+		};
+		bootboxOpts.buttons.confirm.callback = function () {
+			bootbox.alert('Çalışan Detayı');
+		};
+		bootboxOpts.buttons.cancel.callback = function () {
+			$state.go('home', {})
+		};
+		bootbox.dialog(bootboxOpts);				
+        
+    };
     
 }]);
 
