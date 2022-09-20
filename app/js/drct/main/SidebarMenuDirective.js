b674896a2a41fdd85ef0398d9148dd18a3ab6af8
oozcan <onurozcan95@gmail.com>
2022-09-20 12:20:22 -0700

    MAN-28 SidebarMenuDirective yapılması

---
+++ app/js/drct/main/SidebarMenuDirective.js
@@ -0,0 +1,17 @@
+myApp.directive('sidebarMenu', function(){
+
+    return {
+        restrict : "EA",
+        scope: {
+
+        },
+        templateUrl: '../../../view/drct/main/sidebar-menu-directive.html',
+        controller: [
+            '$scope',
+            ($scope) => {
+
+
+            }
+        ]
+    }
+});
