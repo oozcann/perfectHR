ca4ec238706aed3c015eb9b21879e04c4f27bf3d
oozcan <onurozcan95@gmail.com>
2022-09-28 10:34:48 -0700

    MAN-55 Employee liste ekranında Yeni çalışan butonu görünmemektedir

--- app/js/drct/core/NewEntityButtonDirective.js
+++ app/js/drct/core/NewEntityButtonDirective.js
@@ -1,16 +1,19 @@
-myApp.directive('newCompanyButton', function(){
+myApp.directive('newEntityButton', function(){
     return {
         restrict : "EA",
         scope: {
-
+            entity: '@'
         },
-        templateUrl: '../../../view/drct/core/new-company-button-directive.html',
+        templateUrl: '../../../view/drct/core/new-entity-button-directive.html',
         controller: [
             '$scope',
             '$state',
             ($scope,$state) => {
                 $scope.goToNewCompanyPage = function () {
                     $state.go('new-company',{});
+                };
+                $scope.goToNewEmployeePage = function () {
+                    $state.go('new-employee',{});
                 };
             }
         ]
