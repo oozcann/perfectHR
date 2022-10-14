a007b2eb8014d55955c9e0f83f1dcdb0211ced61
oozcan <onurozcan95@gmail.com>
2022-10-14 11:11:38 -0700

    MAN-33 save,update,delete işlemlerinin tek bir directive üzerinden yapılması (abstractNewEditView)

--- app/js/drct/newEditView/CompanyDirective.js
+++ app/js/drct/newEditView/CompanyDirective.js
@@ -19,39 +19,6 @@ myApp.directive('companyDirective', function(){
             ($scope,$state,$stateParams,$http,$rootScope,entityService) => {
                 $scope.getEntityAddress = 'company';
                 $scope.entity = $scope.company;
-                $scope.saveCompany = function (data) {
-                    if ($scope.isNew) {
-                        entityService.saveEntity($scope.getEntityAddress ,JSON.stringify(data)).then((data) => {
-                            $scope.companyId = data._id;
-                            $scope.redirectAfterSave();
-                        });
-                    } else {
-                        entityService.updateEntity($scope.getEntityAddress, JSON.stringify(data)).then(() => {
-                            $scope.beingEdited = false;
-                        });
-
-                    }
-
-                };
-                $scope.deleteCompany = function () {
-                    const bootboxOpts = {};
-                    bootboxOpts.title = 'Devam etmek için onayınız gerekmektedir';
-                    bootboxOpts.message = 'Firmayı arşivlemek istediğinize emin misiniz? Arşivlenmiş firmalara, firma liste ekranından tekrar ulaşarak aktif edebilir veya kalıcı olarak silebilirsiniz.';
-                    bootboxOpts.size = 'large'
-                    bootboxOpts.closeButton = false;
-
-                    bootboxOpts.buttons = {
-                        cancel: { label: 'Vazgeç', className: 'btn-success btn-square' },
-                        confirm: { label: 'Arşivle', className: 'btn-danger btn-square' }
-                    };
-                    bootboxOpts.buttons.confirm.callback = function () {
-                        entityService.deleteEntity($scope.getEntityAddress, JSON.stringify($scope.entity)).then(function (response) {
-                            $state.go('companies', {});
-                        });
-                    };
-                    bootboxOpts.buttons.cancel.callback = function () {};
-                    bootbox.dialog(bootboxOpts);
-                }
                 $scope.removeCompany = function () {
                     const bootboxOpts = {};
                     bootboxOpts.title = 'Devam etmek için onayınız gerekmektedir';
@@ -75,23 +42,6 @@ myApp.directive('companyDirective', function(){
                     entityService.activateEntity($scope.getEntityAddress, JSON.stringify($scope.entity)).then(() => {
                         $state.go($state.current, {}, {reload: true});
                     });
-                };
-                $scope.edit = function() {
-                    $scope.beingEdited = true;
-                }
-                $scope.cancel = function () {
-                    if ($scope.isNew) {
-                        $state.go('companies');
-                    } else {
-                        $scope.beingEdited = false;
-                        $state.go($state.current, {}, {reload: true});
-                    }
-                };
-                $scope.redirectAfterSave = function () {
-                    $state.go('company', {
-                        companyId: $scope.companyId,
-                        justSaved: true
-                    })
                 };
             }
         ]
--- app/js/drct/newEditView/ReminderDirective.js
+++ app/js/drct/newEditView/ReminderDirective.js
@@ -12,63 +12,9 @@ myApp.directive('reminderDirective', function(){
         templateUrl: '../../../view/drct/newEditView/reminder-directive.html',
         controller: [
             '$scope',
-            '$state',
-            '$stateParams',
-            '$http',
-            '$rootScope',
-            'entityService',
-            ($scope,$state,$stateParams,$http,$rootScope,entityService) => {
+            ($scope) => {
                 $scope.getEntityAddress = 'reminder';
                 $scope.entity = $scope.reminder;
-                $scope.saveReminder = function (data) {
-                    if ($scope.isNew) {
-                        entityService.saveEntity($scope.getEntityAddress ,JSON.stringify(data)).then((data) => {
-                            $scope.reminderId = data._id;
-                            $scope.redirectAfterSave();
-                        });
-                    } else {
-                        entityService.updateEntity($scope.getEntityAddress, JSON.stringify(data)).then(() => {
-                            $scope.beingEdited = false;
-                        });
-
-                    }
-                };
-                $scope.deleteReminder = function () {
-                    const bootboxOpts = {};
-                    bootboxOpts.title = 'Devam etmek için onayınız gerekmektedir';
-                    bootboxOpts.message = 'Hatırlatıcıyı silmek istediğinize emin misiniz?';
-                    bootboxOpts.size = 'large'
-                    bootboxOpts.closeButton = false;
-
-                    bootboxOpts.buttons = {
-                        cancel: { label: 'Vazgeç', className: 'btn-success btn-square' },
-                        confirm: { label: 'Sil', className: 'btn-danger btn-square' }
-                    };
-                    bootboxOpts.buttons.confirm.callback = function () {
-                        entityService.deleteEntity($scope.getEntityAddress, JSON.stringify($scope.entity)).then(function (response) {
-                            $state.go('reminders', {});
-                        });
-                    };
-                    bootboxOpts.buttons.cancel.callback = function () {};
-                    bootbox.dialog(bootboxOpts);
-                };
-                $scope.editReminder = function () {
-                    $scope.beingEdited = true;
-                };
-                $scope.cancel = function () {
-                    if ($scope.isNew) {
-                        $state.go('reminders');
-                    } else {
-                        $scope.beingEdited = false;
-                        $state.go($state.current, {}, {reload: true});
-                    }
-                };
-                $scope.redirectAfterSave = function () {
-                    $state.go('reminder', {
-                        reminderId: $scope.reminderId,
-                        justSaved: true
-                    })
-                };
             }
         ]
     }
