b74f8a91388df10ed21f65137c419b6508a121fb
oozcan <onurozcan95@gmail.com>
2022-10-19 12:36:41 -0700

    MAN-44 Çalışan detay ekranında düzenle kaydet yaptıktan sonra sayfa yenilenmeden tarihler gözükmemektedir

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,13 +1,14 @@
 angular.module('myApp.filters', [])
 .filter('formatDateToDDMMYYYY', () => (date) => {
-    if (date && date != null && date != '') {
-        let day = date.slice(8, 10);
-        const dateArray = date.split('-',2);
-        dateArray.push(day);
-        return dateArray[2] + '/' + dateArray[1] + '/' + dateArray[0];
-    } else {
-        return '';
+    if (!date) {
+        return 'Belirtilmemiş';
     }
+    let dateToFormat = moment(date);
+    if (dateToFormat.isValid()) {
+        dateToFormat = moment(date).format('DD/MM/YYYY');
+    }
+    return dateToFormat;
+
 })
 .filter('age', [
 	() => (dateValue) => {
