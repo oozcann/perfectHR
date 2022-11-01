cd7b6386fbc8b08ace7943d6261443b8d1745cfe
oozcan <onurozcan95@gmail.com>
2022-11-01 11:25:22 -0700

    MAN-89 html-form-element eklemeleri

--- app/js/drct/newEditView/ReminderDirective.js
+++ app/js/drct/newEditView/ReminderDirective.js
@@ -16,17 +16,9 @@ myApp.directive('reminderDirective', function(){
             ($scope,referenceService) => {
                 $scope.getEntityAddress = 'reminder';
                 $scope.entity = $scope.reminder;
-                if (!$scope.isNew) {
-                    $scope.reminderCompanyRef = $scope.reminder.companyRef._id;
-                }
                 if (!$scope.isNew && $scope.reminder.reminderDate) {
                     $scope.reminder.reminderDate = new Date($scope.reminder.reminderDate);
                 }
-                $scope.createCompanyRef = function (companyRefId) {
-                    referenceService.createEntityRef('company', companyRefId).then((data) => {
-                        $scope.reminder.companyRef = data;
-                    });
-                };
             }
         ]
     }
